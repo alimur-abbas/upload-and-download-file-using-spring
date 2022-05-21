5a6fd0c276524e845acfe7a6c75641b2ac1e4d38
alimur-abbas <alimurabbas8658402111@gmail.com>
2022-05-21 23:14:37 +0530

    spring-security added

---
+++ src/main/java/com/example/filedemo/init/FlywayConfig.java
@@ -0,0 +1,28 @@
+package com.example.filedemo.init;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+@Component
+public class FlywayConfig
+{
+    @Value("${flyway.script.location}")
+    private String flyWayLocation;
+
+    @Autowired
+    private DataSource dataSource;
+
+
+    @PostConstruct
+    public void afterPropertiesSet() throws Exception {
+        Flyway flyway =Flyway.configure().locations(flyWayLocation).dataSource(dataSource).load();
+        flyway.repair();
+        flyway.migrate();
+    }
+
+}
